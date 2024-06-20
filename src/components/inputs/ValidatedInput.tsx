dd220241e647d55c824c1ad8fbe6bb5080d6a5c9
arrdix <mail.ardix@gmail.com>
2024-06-20 11:52:10 +0700

    feat: implement show/hide password

--- src/components/inputs/ValidatedInput.tsx
+++ src/components/inputs/ValidatedInput.tsx
@@ -1,4 +1,6 @@
-import { Box, Collapse, Input, Text } from '@chakra-ui/react'
+import GhostButton from '@/components/buttons/GhostButton'
+import { Collapse, Input, InputGroup, InputRightElement, Text } from '@chakra-ui/react'
+import { useState } from 'react'
 import { FieldError, UseFormRegister, FieldValues, Path } from 'react-hook-form'
 
 interface ValidatedInputProps<T extends FieldValues> {
@@ -14,23 +16,33 @@ function ValidatedInput<T extends FieldValues>(props: ValidatedInputProps<T>) {
     const { type, placeholder, name, error, register } = props
     const isCollapsed = error ? true : false
 
+    const [showPassword, setShowPassword] = useState(false)
+    const togglePassword = () => setShowPassword(!showPassword)
+
     return (
-        <Box>
+        <InputGroup>
             <Input
                 autoFocus={props.autoFocus && true}
                 id={name}
-                type={type}
+                type={type !== 'password' ? type : showPassword ? 'text' : type}
                 variant={'hollow'}
                 placeholder={placeholder}
                 {...register(name)}
             />
+            {type === 'password' && (
+                <InputRightElement width="4.5rem">
+                    <GhostButton color={'circle.dark'} onClick={togglePassword}>
+                        {showPassword ? 'Hide' : 'Show'}
+                    </GhostButton>
+                </InputRightElement>
+            )}
 
             <Collapse in={isCollapsed} transition={{ enter: { duration: 0.5 } }}>
                 <Text mt={'.5rem'} color={'circle.error'}>
                     {error && error.message}
                 </Text>
             </Collapse>
-        </Box>
+        </InputGroup>
     )
 }
 

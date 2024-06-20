b667068bbe41a0d28c2520fd81900fd14985ea20
arrdix <mail.ardix@gmail.com>
2024-06-20 14:12:43 +0700

    fix: fix vibe reply behavior

--- src/components/vibes/VibeList.tsx
+++ src/components/vibes/VibeList.tsx
@@ -7,13 +7,18 @@ import EmptyMessage from '@/components/utils/EmptyMessage'
 
 interface VibeListProps {
     vibes: VibeType[]
+    noLink?: boolean
 }
 
-function VibeList({ vibes }: VibeListProps) {
+function VibeList({ vibes, noLink }: VibeListProps) {
     if (vibes.length) {
         return (
             <Box>
                 {vibes.map((vibe) => {
+                    if (noLink) {
+                        return <VibeItem vibe={vibe} noHover />
+                    }
+
                     return (
                         <Link to={`/vibe/${vibe.id}`} key={vibe.id}>
                             <VibeItem vibe={vibe} />
